5322e286551a57ad3f1cdb6fef2b5f0ec38ae365
Rahul Sah <rahulgear5ce@gmail.com>
2025-12-22 22:25:23 +0530

    Add files via upload

---
+++ Lab_1/Add_Two.java
@@ -0,0 +1,10 @@
+public class Add_Two {
+    
+    public static void main(String[] args)
+    {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
---
+++ Lab_1/Mutiply.java
@@ -0,0 +1,11 @@
+public class Mutiply {
+    
+    public static void main(String[] args)
+    {
+        int a = 6;
+        int b = 5;
+        int mul = a * b;
+
+        System.out.println(mul);
+    }
+}
