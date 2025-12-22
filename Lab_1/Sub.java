7517915991b8ce20630792c4086e0c546c783b10
Rahul Sah <rahulgear5ce@gmail.com>
2025-12-22 22:51:37 +0530

    Add files via upload

---
+++ Lab_1/Sub.java
@@ -0,0 +1,10 @@
+public class Sub
+{
+    public static void main(String[] args)
+    {
+        int a = 7 ;
+        int b = 4;
+        int sub = a -  b;
+        System.out.println(sub);
+    }
+}
